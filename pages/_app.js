3de36a7f4a2425fa379352c2691973c72a5dcd8d
Godfrey O <40922747+osasere1m@users.noreply.github.com>
2023-05-30 13:08:59 +0100

    first commit
    this is the commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
+import { createTheme } from "@nextui-org/react";
+import Defiswap from './defiswap';
+import { Web3Button } from '@web3modal/react';
+import Swap from './swap';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const darkTheme = createTheme({
+  type: 'dark',
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+      {/*<Web3Button />*/}
+      <Defiswap />
+      <Swap/>
+    </NextUIProvider>
+  );
 }
+
+export default MyApp;
